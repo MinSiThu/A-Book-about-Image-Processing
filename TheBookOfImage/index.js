54b6fda00b73d46a4ec703ba1b94393acf7cb015
Min Si Thu <archimedes557@gmail.com>
2019-08-17 00:08:46 +0630

    The Book Of Image PDF

--- TheBookOfImage/index.js
+++ TheBookOfImage/index.js
@@ -2,10 +2,11 @@ let markdownpdf = require("markdown-pdf")
 let fs = require("fs")
  
 fs.readdir("./",(err,dirs)=>{
-    let book = "/TheBookOfImage.pdf";
+    let bookPath = "TheBookOfImage.pdf";
     let mdDocs = dirs.filter(dir=>{
         return dir.includes(".md");
     })
+    
     markdownpdf().concat.from(mdDocs).to(bookPath, function () {
         console.log("Created", bookPath)
     })
